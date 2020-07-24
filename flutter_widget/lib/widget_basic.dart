fe6d863951de86b29c90cf6e218d2e5c4647b4d1
davidtran <davidtran641@gmail.com>
2020-07-24 22:31:45 +0800

    (FEAT) Build tabbar

--- flutter_widget/lib/widget_basic.dart
+++ flutter_widget/lib/widget_basic.dart
@@ -3,29 +3,53 @@ import 'package:flutter/material.dart';
 class BasicWidgetDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Basic widget')),
-      body: Container(
-        height: 56.0,
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(color: Colors.blue[500]),
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Navigation menu',
-              onPressed: null,
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text('Basic widgets'),
+          )
+          ,
+          Expanded(
+            child: Center(
+              child: Text('Hello widgets!'),
             ),
-            Expanded(
-              child: Text('Demo widgets'),
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            ),
-          ],
-        ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: () {},
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
       ),
     );
   }
