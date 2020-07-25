e8f2c04bebd8438d75603b9b6a177aacab7712a0
davidtran <davidtran641@gmail.com>
2020-07-25 11:27:49 +0800

    (FEAT) Handle highlight state

--- flutter_widget/lib/state_manage_demo.dart
+++ flutter_widget/lib/state_manage_demo.dart
@@ -27,32 +27,64 @@ class _StateManageDemoState extends State<StateManageDemo> {
   }
 }
 
-class TapBox extends StatelessWidget {
+class TapBox extends StatefulWidget {
   TapBox({Key key, this.isActive: false, @required this.onChanged})
       : super(key: key);
 
   final bool isActive;
   final ValueChanged<bool> onChanged;
 
+  @override
+  _TapBoxState createState() => _TapBoxState();
+}
+
+class _TapBoxState extends State<TapBox> {
+  var _isHighlight = false;
+
   void _handleTap() {
-    onChanged(!isActive);
+    widget.onChanged(!widget.isActive);
+  }
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _isHighlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _isHighlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _isHighlight = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _handleTap,
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
       child: Container(
         child: Center(
           child: Text(
-            isActive ? 'Active' : 'Inactive',
+            widget.isActive ? 'Active' : 'Inactive',
             style: TextStyle(fontSize: 32, color: Colors.white),
           ),
         ),
         width: 200,
         height: 200,
-        decoration:
-            BoxDecoration(color: isActive ? Colors.green : Colors.grey[500]),
+        decoration: BoxDecoration(
+          color: widget.isActive ? Colors.green : Colors.grey[500],
+          border: _isHighlight
+              ? Border.all(color: Colors.teal[700], width: 10)
+              : null,
+        ),
       ),
     );
   }
